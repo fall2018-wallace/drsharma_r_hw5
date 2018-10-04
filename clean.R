7a0d5c6bbc7168978e4e9ab3a9a35a9d19bc6916
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 12:59:53 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -6,13 +6,11 @@ readStates <- function(states)
 #removing rows that are not needed
 # -- first row is the total for the US, we do not need that
     states <- states[-1,]
-    
-    
-    
+#last row is not a state so we don't need that    
     num.row <- nrow(states)
     states <- states[-num.row,]
     
-#last row is not a state so we don't need that
+
     states <- states[,-1:-4]
     
     colnames(states) <- c("stateName", "population", "popOver18", "percentOver18")
